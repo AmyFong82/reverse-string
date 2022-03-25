2f1a4bea39e045e0e9ba738581b78dc96e6b8ea8
AmyFong82 <amyhfong@gmail.com>
2022-03-25 10:49:41 -0400

    added pseudocode and explanation.

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,8 +1,8 @@
 def reverse_string(str)
   # type your code in here
-  str_length = str.size
   str_arr = str.chars
   reversed_str = []
+  str_length = str.size
   counter = str_length - 1
   for c in str_arr do
     reversed_str[counter] = c
@@ -14,6 +14,12 @@ end
 reverse_string("amyBilal")
 
 if __FILE__ == $PROGRAM_NAME
+
+  puts "Expecting: 'laliByma'"
+  puts '=>', reverse_string("amyBilal")
+
+  puts 
+
   puts "Expecting: 'ih'"
   puts "=>", reverse_string('hi')
 
@@ -22,8 +28,13 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ybabtac'"
   puts "=>", reverse_string('catbaby')
 
-  # Don't forget to add your own!
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+1. First, turn the str into an array, so that we can iterate over it.
+2. Make an empty array to hold the final versed string array.
+3. Find the length of the string.
+4. Make a counter that is 1 less than the length of the string, for the indexing.
+5. Iterate over the str array, and putting the first one to the last, as going forward as the counter counts from the last index to 0 (first one).
+6. Join the array back to a string.

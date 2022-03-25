7b7f2f9117b5ac37c262bcc2b370b7b85505b598
AmyFong82 <amyhfong@gmail.com>
2022-03-25 10:40:08 -0400

    working code for the reversed_string in rb.

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,17 @@
 def reverse_string(str)
   # type your code in here
+  str_length = str.size
+  str_arr = str.chars
+  reversed_str = []
+  counter = str_length - 1
+  for c in str_arr do
+    reversed_str[counter] = c
+    counter -= 1
+  end
+  reversed_str.join
 end
+
+reverse_string("amyBilal")
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
